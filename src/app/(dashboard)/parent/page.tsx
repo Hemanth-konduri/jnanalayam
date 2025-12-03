a56c5a2fcba5dba6f5ecb5f8069a1b245db539d1
Hemanth-konduri <kondurihemanth62@gmail.com>
2025-12-03 16:27:41 +0530

    created student teacher and parent dashboards

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,7 +1,25 @@
+import Announcements from "@/components/Announcements"
+import BigCalendar from "@/components/BigCalender"
+
+
 const ParentPage = () => {
   return (
-    <div>Parent page</div>
+    <div className="flex-1 p-4 flex gap-4 flex-col xl:flex-row">
+      {/* LEFT */}
+      <div className="w-full xl:w-2/3 flex flex-col gap-8">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule for John doe(4A)</h1>
+          <BigCalendar/>
+        </div>
+      </div>
+      {/* RIGHT */}
+       <div className="flex flex-col w-full gap-8 xl:w-1/3">
+         
+          <Announcements/>
+      </div>
+
+    </div>
   )
 }
 
-export default ParentPage
+export default ParentPage;
